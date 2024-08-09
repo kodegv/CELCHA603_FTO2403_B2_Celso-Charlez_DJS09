d988563ce2e9325653888bea97c805e5f26b2682
kodegv <celsocharlez1@gmail.com>
2024-08-09 11:37:09 +0200

    Created loop to add properties

--- index.ts
+++ index.ts
@@ -118,3 +118,14 @@ const properties: Property[] = [
 showReviewTotal(1, reviews[0].name, reviews[0].loyaltyUser);
 
 populateUser(you.isReturning, you.firstName);
+
+// push properties loop
+for (let i = 0; i < properties.length; i++) {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.innerHTML = properties[i].title;
+    const image = document.createElement("img");
+    image.setAttribute("src", properties[i].image);
+    card.appendChild(image);
+    propertyContainer.appendChild(card);
+  }
